576c245809a33bdc527bbb3a7c31e4692996f867
Navajeevan Reddy Gondi <ngondi.reddy@gmail.com>
2017-08-07 08:24:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,11 @@
 console.log('Loaded!');
-alert("heyy hello");
+var img=document.getElementById("mad");
+img.onClick=function(){
+    
+    var time=setInterval(moveRight,50);
+};
+function moveRight(){
+    var s=0;
+    s=img+5;
+    img.style.marginright=s+px;
+}
